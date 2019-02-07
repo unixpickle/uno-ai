e3d112d841e578f179c1e24cbfb41d1391835822
Alex Nichol <unixpickle@gmail.com>
2019-02-07 13:06:06 -0500

    remove redundancy

--- uno_ai/rollouts.py
+++ uno_ai/rollouts.py
@@ -4,15 +4,14 @@ Gather trajectories from agents on a game.
 
 
 class Rollout:
-    def __init__(self, observations, options, outputs, reward):
+    def __init__(self, observations, outputs, reward):
         self.observations = observations
-        self.options = options
         self.outputs = outputs
         self.reward = reward
 
     @classmethod
     def empty(cls):
-        return cls([], [], [], 0.0)
+        return cls([], [], 0.0)
 
     @classmethod
     def rollout(cls, game, agents):
@@ -24,7 +23,6 @@ class Rollout:
                 states[i] = res['state']
                 r = rollouts[i]
                 r.observations.append(game.obs(i))
-                r.options.append(res['options'])
                 r.outputs.append(res)
         for i, r in enumerate(rollouts):
             if i == game.winner():
