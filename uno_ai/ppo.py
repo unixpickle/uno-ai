13d999503fb4c24ace6ca3acf4d982a87250baa5
Alex Nichol <unixpickle@gmail.com>
2019-02-07 14:08:54 -0500

    drafted PPO

---
+++ uno_ai/ppo.py
@@ -0,0 +1,47 @@
+"""
+Proximal Policy Optimization.
+"""
+
+import torch
+import torch.functional as F
+import torch.optim as optim
+
+
+class PPO:
+    def __init__(self, agent, epsilon=0.2, lr=1e-4, ent_reg=1e-2):
+        self.agent = agent
+        self.epsilon = epsilon
+        self.optim = optim.Adam(agent.parameters(), lr=lr)
+        self.ent_reg = ent_reg
+
+    def loop(self, batch, iters=8):
+        steps = []
+        for _ in range(iters):
+            steps.append(self.step(batch))
+
+    def step(self, batch):
+        logits, values, _ = self.agent(batch.observations)
+        masked_logits = logits - (1 - batch.masks) * 10000
+        all_probs = F.log_softmax(masked_logits, dim=-1)
+        log_probs = torch.sum(all_probs * batch.actions, dim=-1)
+
+        vf_loss = torch.mean(torch.pow(values - batch.targets, 2))
+
+        ratio = torch.exp(log_probs - batch.log_probs)
+        clip_ratio = torch.clamp(ratio, 1 - self.epsilon, 1 + self.epsilon)
+        pi_loss = -torch.mean(torch.min(ratio * batch.advs, clip_ratio * batch.advs))
+
+        neg_entropy = torch.sum(torch.exp(log_probs) * log_probs, dim=-1)
+        ent_loss = torch.mean(self.ent_reg * neg_entropy)
+
+        loss = vf_loss + pi_loss + ent_loss
+        self.optim.zero_grad()
+        loss.backward()
+        self.optim.step()
+
+        return {
+            'vf_loss': vf_loss.item(),
+            'pi_loss': pi_loss.item(),
+            'ent_loss': ent_loss.item(),
+            'entropy': -neg_entropy.item(),
+        }
--- uno_ai/rollouts.py
+++ uno_ai/rollouts.py
@@ -79,37 +79,51 @@ class RolloutBatch:
       actions: a (seq_len, batch, ACTION_VECTOR_SIZE)
         Tensor of one-hot vectors indicating which action
         was taken at every timestep.
+      log_probs: the initial log probabilities of the
+        actions.
       masks: a (seq_len, batch, ACTION_VECTOR_SIZE) Tensor
         of values where a 1 indicates that an action was
         allowed, and a 0 indicates otherwise.
       advs: a (seq_len, batch) Tensor of advantages.
+      targets: a (seq_len, batch) Tensor of target values.
     """
 
     def __init__(self, rollouts, device, gamma=0.99, lam=0.95):
         seq_len = max(r.num_steps for r in rollouts)
         observations = []
         actions = []
+        log_probs = []
         masks = []
         advs = []
+        targets = []
         for r in rollouts:
             obs_seq = r.observations.copy()
             act_seq = [_one_hot_action(o['action']) for o in r.outputs]
+            log_seq = [o['log_prob'] for o in r.outputs]
             mask_seq = [_option_mask(o['options']) for o in r.outputs]
             adv_seq = r.advantages(gamma=gamma, lam=lam)
+            targ_seq = [adv + o['value'] for adv, o in zip(adv_seq, r.outputs)]
             for _ in range(seq_len - r.num_steps):
                 obs_seq.append([0] * OBS_VECTOR_SIZE)
                 act_seq.append(_one_hot_action(0))
+                log_seq.append(0)
                 mask_seq.append(_one_hot_action(0))
                 adv_seq.append(0)
+                targ_seq.append(0)
             observations.append(obs_seq)
             actions.append(act_seq)
+            log_probs.append(log_seq)
             masks.append(mask_seq)
             advs.append(adv_seq)
+            targets.append(targ_seq)
         self.observations = torch.from_numpy(np.transpose(
             np.array(observations), axes=[1, 0, 2])).to(device)
         self.actions = torch.from_numpy(np.transpose(np.array(actions), axes=[1, 0, 2])).to(device)
+        self.log_probs = torch.from_numpy(np.transpose(
+            np.array(log_probs), axes=[1, 0, 2])).to(device)
         self.masks = torch.from_numpy(np.transpose(np.array(masks), axes=[1, 0, 2])).to(device)
         self.advs = torch.from_numpy(np.transpose(np.array(advs), axes=[1, 0, 2])).to(device)
+        self.targets = torch.from_numpy(np.transpose(np.array(targets), axes=[1, 0, 2])).to(device)
 
 
 def _one_hot_action(action):
