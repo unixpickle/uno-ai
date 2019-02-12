0f7198822b990f3dc97d407612c1799fd6b2aa5c
Alex Nichol <unixpickle@gmail.com>
2019-02-12 10:40:09 -0500

    better results
    Now gets closer to 0.42 on the baseline vs 0.39

--- uno_ai/agent.py
+++ uno_ai/agent.py
@@ -21,12 +21,11 @@ class Agent(nn.Module):
     def __init__(self):
         super().__init__()
         self.input_proc = nn.Sequential(
-            nn.Linear(OBS_VECTOR_SIZE, 256),
+            nn.Linear(OBS_VECTOR_SIZE, 128),
             nn.Tanh(),
-            nn.Linear(256, 256),
         )
-        self.norm = nn.LayerNorm(256)
-        self.rnn = nn.LSTM(256, 256, num_layers=2)
+        self.norm = nn.LayerNorm(128)
+        self.rnn = nn.LSTM(128, 128)
         self.policy = nn.Linear(256, ACTION_VECTOR_SIZE)
         self.value = nn.Linear(256, 1)
         for param in list(self.policy.parameters()) + list(self.value.parameters()):
@@ -54,12 +53,13 @@ class Agent(nn.Module):
         seq_len, batch = inputs.shape[0], inputs.shape[1]
         flat_in = inputs.view(-1, OBS_VECTOR_SIZE)
         features = self.norm(self.input_proc(flat_in))
-        features = features.view(seq_len, batch, -1)
+        feature_seq = features.view(seq_len, batch, -1)
         if states is None:
-            outputs, (h_n, c_n) = self.rnn(features)
+            outputs, (h_n, c_n) = self.rnn(feature_seq)
         else:
-            outputs, (h_n, c_n) = self.rnn(features, states)
+            outputs, (h_n, c_n) = self.rnn(feature_seq, states)
         flat_out = outputs.view(-1, outputs.shape[-1])
+        flat_out = torch.cat([flat_out, features], dim=-1)
         flat_logits = self.policy(flat_out)
         flat_values = self.value(flat_out)
         logits = flat_logits.view(seq_len, batch, ACTION_VECTOR_SIZE)
